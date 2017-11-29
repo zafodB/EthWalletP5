1d0351b6a303240c9676d41d16798e805a5b72cb
filip <filiplama@gmail.com>
2017-11-29 14:31:16 +0100

    Toast me

--- app/src/main/java/com/example/filip/ethwalletp5/MainActivity.java
+++ app/src/main/java/com/example/filip/ethwalletp5/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.filip.ethwalletp5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button myButton = findViewById(R.id.button1);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "You're ugly.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
